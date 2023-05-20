540e838542157f120a1fe09733e9c816c2e2410a
ap3dominator <ap3dominator@gmail.com>
2023-05-20 13:26:08 +0530

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
 import Home from "./component/Home";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./cutsom.scss";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename={window.location.pathname || ""}>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </BrowserRouter>
+      <Home />
     </>
   );
 }
